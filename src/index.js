b17dbb079fcb8674ae0cc29d9b9d1058b1e3f43e
Archit Nigam <50867638+architn@users.noreply.github.com>
2022-03-03 22:48:56 -0500

    Added other weather parameters

--- src/index.js
+++ src/index.js
@@ -12,20 +12,41 @@ function onSearch(){
             weatherSection.style.display = "block";
             console.log(weatherReportData);
             const locationElement = document.getElementById("location")
-            const weatherElement = document.getElementById("temperature")
-            let weather = ""
+            const temperatureElement = document.getElementById("temperature")
+            const conditionElement = document.getElementById("condition")
+            const windSpeedElement = document.getElementById("wind-speed")
+            const humidityElement = document.getElementById("humidity")
+            const pressureElement = document.getElementById("pressure")
+            const feelsLikeElement = document.getElementById("feels-like")
+            const visibilityElement = document.getElementById("visibility")
+            const cloudyElement = document.getElementById("cloud")
+            const precipitationElement = document.getElementById("precipitation")
+            const uvElement = document.getElementById("uv")
+            
+            let temperature = ""
+            let feelsLike = ""
             let userSelection = document.getElementById("selection").value
             switch(userSelection){
                 case "c":
-                    weather = weatherReportData.current.temp_c + "&deg; C"
+                    temperature = weatherReportData.current.temp_c + "&deg; C"
+                    feelsLike = weatherReportData.current.feelslike_c + "&deg; C"
                     break;
                 case "f":
-                    weather = weatherReportData.current.temp_f + "&deg; F"
+                    temperature = weatherReportData.current.temp_f + "&deg; F"
+                    feelsLike = weatherReportData.current.feelslike_f + "&deg; F"
             }
-            locationElement.innerHTML = weatherReportData.location.name;
-            weatherElement.innerHTML = weather
-            // var weatherDiv = document.getElementById("weather");
-            // weatherDiv.append(weatherInCelsius);
+            locationElement.innerHTML = weatherReportData.location.name + ", "+ weatherReportData.location.region;
+            conditionElement.innerHTML = weatherReportData.current.condition.text
+            temperatureElement.innerHTML = temperature;
+            $("#imageElement").css("src", weatherReportData.current.condition.icon);
+            windSpeedElement.innerHTML = "<strong>Wind Speed:</strong> "+ weatherReportData.current.wind_mph + " mph" + "/ " + weatherReportData.current.wind_kph + " kph"
+            humidityElement.innerHTML = "<strong>Humidity:</strong> "+ weatherReportData.current.humidity + "%" 
+            pressureElement.innerHTML = "<strong>Pressure:</strong> "+ weatherReportData.current.pressure_mb + " mb" + "/ "+ weatherReportData.current.pressure_in + " in"
+            feelsLikeElement.innerHTML = "<strong>Feels Like:</strong> "+feelsLike
+            visibilityElement.innerHTML = "<strong>Visibility:</strong> "+ weatherReportData.current.vis_km + " km/ " + weatherReportData.current.vis_miles + " miles"
+            precipitationElement.innerHTML =  "<strong>Precipitation:</strong> "+ weatherReportData.current.precip_mm + " mm/ " + weatherReportData.current.precip_in + " in"
+            cloudyElement.innerHTML = "<strong>Clouds:</strong> "+ weatherReportData.current.cloud + " %"
+            uvElement.innerHTML = "<strong>UV Radiation:</strong> "+weatherReportData.current.uv + " - Very Good"
         }
     }
     xhr.send();
